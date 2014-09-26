bb80da548d97c11c1a92d6474839233b36bf972d
Lheureux Axel <lheureux@a14p17.fil.univ-lille1.fr>
2014-09-26 16:33:09 +0200

    added turn rotation

--- Game.java
+++ Game.java
@@ -1,10 +1,17 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Game
 {
-    private Board board;
+    private Board		board;
+    private List<Player>	playerList;
+    private int			nextPlayer;
 
     public Game()
     {
 	this.board = new Board();
+	this.playerList = new ArrayList<Player>();
+	this.nextPlayer = 0;
     }
 
     public void startNewGame()
@@ -20,7 +27,7 @@ public class Game
 	this.addPlayer(p1);
 	this.addPlayer(p2);
 	while (!isGameFinished())
-	    playTurn(p1, throwDie() + throwDie());
+	    playTurn(nextPlayer(), throwDie() + throwDie());
     }
 
     public void playTurn(Player p, int diceThrow)
@@ -40,6 +47,13 @@ public class Game
 	    System.out.println(p.getName() + " can't move this turn");
     }
 
+    public Player nextPlayer()
+    {
+	if (this.nextPlayer == this.playerList.size())
+	    this.nextPlayer = 0;
+	return (this.playerList.get(this.nextPlayer++));
+    }
+
     public boolean isGameFinished()
     {
 	return (this.board.getCell(this.board.getBoardSize() - 1).getPlayer() != null);
@@ -49,6 +63,7 @@ public class Game
     {
 	p.setCell(this.board.getCell(0));
 	this.board.getCell(0).welcome(p);
+	this.playerList.add(p);
     }
 
     public static int throwDie()
